7bfb32671f823d9d81970464db1ac83af70112b8
Kevin C Malin <racmalin@yahoo.com>
2020-04-23 00:55:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
- end
+ 
 shipping_manifest["oil paintings"]
 end
 
@@ -24,7 +24,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2, ["gun powder"] = 4
-end
+
 return shipping_manifest
 
 end
