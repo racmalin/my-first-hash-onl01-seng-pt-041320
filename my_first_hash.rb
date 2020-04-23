a6c71615c0f6cf28e16a148b3891ce50a4bafe8a
Kevin C Malin <racmalin@yahoo.com>
2020-04-23 00:59:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] = 2 , ["gun powder"] = 4
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
 end
 return shipping_manifest
 
