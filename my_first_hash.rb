d4a065b566c189d34eea90839ac0e7b05e733bde
Kevin C Malin <racmalin@yahoo.com>
2020-04-23 00:54:36 +0000

    Automatically backed up by Learn

---
+++ my_first_hash.rb
@@ -0,0 +1,30 @@
+def my_hash
+  my_hash = {"my_name" => "Bob"}
+end
+
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+end
+
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+
+ end
+shipping_manifest["oil paintings"]
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+  shipping_manifest["muskets"] = 2, ["gun powder"] = 4
+end
+return shipping_manifest
+
+end
